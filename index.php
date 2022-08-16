1006e6a6d6109343f5e13dab90ef3a9769872789
valxturu <raival331@gmail.com>
2022-08-16 12:22:55 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis, optio! Vitae ea dolorem officia officiis. Distinctio recusandae voluptate enim nisi, aliquam iusto tenetur ducimus culpa ex fugit fugiat sapiente unde.</p>
+<ul>
+    <li>satu</li>
+    <li>dua</li>
+</ul>
 </body>
 </html>
